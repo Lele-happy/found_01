d7c2ee81be823f57ecdb8fa7a6ab2d570ab673f2
lele-happy <2205379360@qq.com>
2021-05-16 19:17:42 +0800

    提交文件

---
+++ pages/about/about.js
@@ -0,0 +1,14 @@
+// pages/about/about.js
+Page({
+data:{
+  loadingHide:true,
+  loadingText:"加载中"
+},
+onLoad:function(options){
+  var that = this;
+  that.setData({loadingHide:false});
+  setTimeout(function(){
+    that.setData({loadingHide:true});
+  },1000)
+}
+})
